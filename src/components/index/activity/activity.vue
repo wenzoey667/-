b274ea438f87c3b9cf02808d0eaa61e40315dd4c
yangjiayoouonv <1073515964.@qq.com>
2018-11-21 12:55:54 +0800

    e

--- src/components/index/activity/activity.vue
+++ src/components/index/activity/activity.vue
@@ -1,12 +1,33 @@
 <template>
 	<div>
-		这是活动
+		<mt-swipe v-if="contents.data" :auto="0">
+			<mt-swipe-item v-for="datas,index in contents.data.modules[0].moduleContent.banners" :key="datas.id">
+				<img :src="datas.bannerImgSrc" alt="">
+			</mt-swipe-item>
+		</mt-swipe>
 	</div>
 </template>
 
 <script>
+	import axios from "axios";
+	import { Swipe, SwipeItem } from 'mint-ui';
+	import 'mint-ui/lib/style.css';
 	export default {
 		name: 'activity',
+		data(){
+			return{
+				contents:{}
+			}
+		},
+		components:{
+			'swipe':Swipe,
+			'swipe-item':SwipeItem
+		},
+		mounted(){
+			if(this.$store.state.indexData.length==0){
+				axios.get('')
+			}
+		}
 	}
 </script>
 
