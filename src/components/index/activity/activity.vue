1744a9ca300c87dbed6fde785c4e6c6038c4525b
yangjiayoouonv <1073515964.@qq.com>
2018-11-22 19:30:04 +0800

    ycl.8

--- src/components/index/activity/activity.vue
+++ src/components/index/activity/activity.vue
@@ -1,5 +1,5 @@
 <template>
-	<div v-if="contents.data" >
+	<div v-if="contents.data">
 		<mt-swipe :auto="0">
 			<mt-swipe-item v-for="datas,index in contents.data.modules[0].moduleContent.banners" :key="datas.id">
 				<img :src="datas.bannerImgSrc" alt="">
@@ -60,6 +60,11 @@
 			'swipe-item':SwipeItem
 		},
 		mounted(){
+			axios.get('/v2/page?pageId=1&tabId=10010&_='+new Date().getTime()).then(resp=>{
+					this.contents = resp.data;
+					this.showArr = this.contents.data.modules[6].moduleContent.products.slice(0,6);
+					this.$store.state.activeData = resp.data;
+			})
 			window.addEventListener('scroll',()=>{
 				if (document.documentElement.scrollTop > 400) {
 					this.isshow = true
@@ -67,7 +72,7 @@
 					this.isshow = false
 				}
 			})
-			if(this.$store.state.activeData.length===0){
+			/*if(this.$store.state.activeData.length===0){
 				axios.get('/v2/page?pageId=1&tabId=10010&_='+new Date().getTime()).then(resp=>{
 					this.contents = resp.data;
 					this.showArr = this.contents.data.modules[6].moduleContent.products.slice(0,6);
@@ -75,7 +80,7 @@
 				})
 			}else{
 				this.contents = this.$store.state.activeData;
-			}
+			}*/
 		}
 	}
 </script>
