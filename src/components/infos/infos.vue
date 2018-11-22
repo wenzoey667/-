b0f34e969f5c5cbedb0c9cf8f93e03f32a72c6ba
yangjiayoouonv <1073515964.@qq.com>
2018-11-22 19:19:28 +0800

    ycl.8

--- src/components/infos/infos.vue
+++ src/components/infos/infos.vue
@@ -5,7 +5,23 @@
 			<h3 class="Two" slot="Two">消息</h3>
 			<i class="iconfont icon-wode Three" slot="Three" @click="theLast"></i>
 		</nav>
-
+		<content>
+			<div class="top clear">
+				<div class="icon left">
+					<i class="iconfont icon-tongzhi"></i>
+				</div>
+				<div class="Wenzi left clear">
+					<div class="clear">
+						<p class="wen left">尖叫精选</p>
+						<p class="youjian right" @click="handleclick()">＞</p>
+					</div>
+					<div>
+						<p class="neirong" @click="handleCl()">20%靠宜家，70%靠网淘，12万装出138m²日系阳光的家！</p>
+						<p class="shijian">3小时前</p>
+					</div>
+				</div>
+			</div>
+		</content>
 	</div>
 </template>
 
@@ -21,10 +37,13 @@
 			},
 			theLast(){
 				this.$router.push('/mine')
+			},
+			handleCl(){
+				this.$router.push('/contents')
+			},
+			handleclick(){
+				this.$router.push('/xijie')
 			}
-		},
-		mounted(){
-			axios.get()
 		}
 
 	}
@@ -35,6 +54,7 @@
 		position: relative;
 		height: 44px;
 		line-height: 44px;
+		border-bottom: 1px solid #f5f5f5;
 	}
 	.One{
 		position: fixed;
@@ -49,6 +69,46 @@
 		position: fixed;
 		top: 0;
 		right: 10px;
+	}
+	.zhanwei{
+		height: 20px;
+	}
+	content{
+		padding: 20px 25px 20px 25px;
+	}
+	.top{
+		overflow: hidden;
+	}
+	.icon{
+		margin: 0 30px 0 25px;
+		font-size: 18px;
+		height: 40px;
+		line-height: 40px;
+	}
+	.Wenzi{
+		font-size: 18px;
+		width: 300px;
+	}
+	.wen{
+		border-bottom: 1px solid #f5f5f5;
+		height: 40px;
+		line-height: 40px;
+	}
+	.youjian{
+		height: 40px;
+		line-height: 40px;
+	}
+	.neirong{
+		color: #65686C;
+		font-size: 12px;
+		height: 34px;
+		margin: 13px 0;
+	}
+	.shijian{
+		color: #65686C;
+		font-size: 12px;
+		height: 34px;
+		margin: 13px 0;
 	}
 </style>
 <style>
