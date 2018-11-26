4e864bbba0db44216b3d6f45f52ef698a750328e
liwenjing <1104948685@qq.com>
2018-11-26 08:06:08 +0800

    w

--- src/components/mine/login.vue
+++ src/components/mine/login.vue
@@ -1,20 +1,23 @@
 <template>
-	<div>
+	<div class="container">
 		<header>
-			<i class="iconfont icon-shouye b" ></i>
+			<i @click="home()" class="iconfont icon-shouye b" ></i>
 			<h2>登录</h2>
-			<span class="a">注册</span>
+			<span @click="register()" class="a">注册</span>
 		</header>
 		<div class="box">
 			<div class="mode">
 				<ul>
-					<li>密码登录</li>
-					<li>短信登录</li>
+					<router-link activeClass="on" to="/login" tag="li">密码登录</router-link>
+					<router-link activeClass="on" to="/login/#" tag="li">短信登录</router-link>
 				</ul>
 			</div>
 			<input type="text" name="username" ref="username" placeholder="请输入用户名"><br>
-			<input type="password" name="password" ref="password" placeholder="请输入密码"><br>
-			<input type="submit" @click="handleClick()">
+			<input type="password" name="password" ref="password" placeholder="请输入密码"><span>忘记密码？</span><br>
+		</div>
+		<button type="submit" @click="handleClick()" class="ab">确定</button>
+		<div class="bottom">
+			<span class="left">一个月内记住登录</span><span class="right">快速注册</span>
 		</div>
 	</div>
 </template>
@@ -39,12 +42,19 @@
 				    'password':this.$refs.password.value
 				  }
 				}).then((response)=>{
-				  console.log(response.data)
 				  this.$store.state.isLogin = response.data;
-				  console.log(this.$store.state.isLogin);
+				  if(this.$store.state.isLogin){
+				  	this.$router.push('/mine');
+				  }
 				}).catch(err=>{
 					console.log(err);
 				});
+			},
+			home(){
+				this.$router.push('/index/home');
+			},
+			register(){
+				this.$router.push('/register');
 			}
 		},
 		mounted(){
@@ -53,6 +63,12 @@
 	}
 </script>
 <style scoped lang="scss">
+	.container{
+		position: relative;
+		background: #f5f5f5;
+		width:100%;
+		height: 700px;
+	}
 	header{
 	    background: #fff;
 	    height: 45px;
@@ -77,15 +93,77 @@
 		}
 	}
 	.box{
-	    width: 400px;
+		margin-left: 13px;
+	    width: 350px;
 	    background-color: #fff;
-	    margin: 0 15px;
+	    position: relative;
 	    margin-top: 15px;
 	    height: 170px;
+	    overflow: hidden;
 	    .mode{
 	    	ul{
-
+	    		padding-top: 15px;
+			    background: #FCFCFC;
+			    height: 44px;
+			    overflow: hidden;
+			    li{
+		    	    float: left;
+	    	        margin: 0 45px;
+				    font-size:15px;
+				    padding: 0 12px;
+				    color:#808080;
+			    }
+			    .on{
+		    	    border-bottom: 3px solid #000;
+				    color: #000;
+				    height: 44px;
+			    }
 	    	}
 	    }
+    	input{
+		    width: 100%;
+		    height: 50px;
+		    padding:0 10px;
+		    box-sizing: border-box;
+		    // border-bottom: 1px solid #EAEAEA;
+		    border:1px solid white;
+		    font-size: 14px;
+		    color: #000;
+		    font-family: 'PingFangSC-Regular';
+		    outline: none;
+    	}
+    	input:nth-of-type(1){
+    		border-bottom:1px solid #ccc;
+    		background-clip: content-box;
+    	}
+    	span{
+    		position: absolute;
+    		display: block;
+    		right: 20px;
+    		font-size: 15px;
+    		top:125px;
+    	}
 	}
+	.ab{
+    		margin: 0 auto;
+		    vertical-align: middle;
+		    width: 340px;
+		    height: 44px;
+		    background-color: #FFD444;
+		    font-size: 14px;
+		    position: absolute;
+		    top: 240px;
+		    margin-left: 18px;
+    	}
+    	.bottom{
+    		margin-top: 70px;
+    		.left{
+				margin-left: 20px;
+				font-size: 14px;
+    		}
+    		.right{
+				margin-right: 20px;	
+				font-size: 13px;
+    		}
+    	}
 </style>
